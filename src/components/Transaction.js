5cd271b35d500ac7a4d995ffc53e3803445f9d1e
Fificode <lamidifirdaws@gmail.com>
2022-03-30 02:34:43 +0100

    first commit

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,21 +1,20 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import { GlobalContext } from '../context/Globalstate'
 
 const Transaction = ({transaction}) => {
-  const [style, setStyle] = useState({display: 'none'});
+ 
     const {deleteTransaction} = useContext(GlobalContext);
     const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
     <>
 <li className={transaction.amount < 0 ? 'minus' : 'plus'}
-   onMouseEnter = {e => {setStyle({display: 'block'})}}
-  onMouseLeave = {e => {setStyle({display: 'none'})}}>
+  >
   
                   {transaction.text}
          <span className='transaction-value'>{sign}${Math.abs(transaction.amount)}
           </span>
-               <button className='delete-btn' onClick = {() => deleteTransaction(transaction.id)} style={style}>x</button>
+               <button className='delete-btn' onClick = {() => deleteTransaction(transaction.id)} >x</button>
             </li>
     </>
   )
